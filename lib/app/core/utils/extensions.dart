6ed682dde5929e1cef8542b9e0d7c8aa0574ae8d
VinayaAugusthy <vinayaaugusthy@gmail.com>
2024-02-02 17:38:18 +0530

    extension file created

--- lib/app/core/utils/extensions.dart
+++ lib/app/core/utils/extensions.dart
@@ -3,8 +3,11 @@ import 'package:get/get.dart';
 
 extension PercentSized on double {
   double get hp => (Get.height * (this / 100));
-    double get wp => (Get.width * (this / 100));
+  double get wp => (Get.width * (this / 100));
+}
 
+extension ResposiveText on double {
+  double get sp => (Get.width/100 * (this / 3));
 }
 
 extension HexColor on Color {
